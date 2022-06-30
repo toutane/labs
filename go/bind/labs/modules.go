c76544d942fbe8b14ea8cc8e44b65d8ae69de2e4
toutane <ca@leger.email>
2022-06-30 11:40:17 +0200

    feat(modules): create github-api module

--- go/bind/labs/modules.go
+++ go/bind/labs/modules.go
@@ -4,8 +4,10 @@ import (
 	"berty.tech/labs/go/pkg/blmod"
 
 	"berty.tech/labs/go/mod/datastorebench"
+	"berty.tech/labs/go/mod/github_api"
 )
 
 var modules = []blmod.ModuleFactory{
 	datastorebench.New,
+	github_api.New,
 }
